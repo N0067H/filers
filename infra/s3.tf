e9c34228ea54914dbe6ded2e394a052ac5b92646
N0067H <n006h3417h@gmail.com>
2026-08-06 15:44:53 +0900

    feat(infra): add s3 lifecycle config

--- infra/s3.tf
+++ infra/s3.tf
@@ -6,6 +6,23 @@ resource "aws_s3_bucket" "file_storage" {
     bucket = var.bucket_name
 }
 
+resource "aws_s3_bucket_lifecycle_configuration" "file_storage" {
+  bucket = aws_s3_bucket.file_storage.id
+
+  rule {
+    id     = "abort-incomplete-multipart-uploads"
+    status = "Enabled"
+
+    filter {
+      prefix = "files/"
+    }
+
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 7
+    }
+  }
+}
+
 output "bucket_name" {
     value = aws_s3_bucket.file_storage.bucket
 }
